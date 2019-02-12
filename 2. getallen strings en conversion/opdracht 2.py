a7b28684ebc700f6bbfb5538a143d173b531f661
Milot <milot.makelaar@student.hu.nl>
2019-02-12 12:22:32 +0100

    opdracht 2

--- 2. getallen strings en conversion/opdracht 2.py
+++ 2. getallen strings en conversion/opdracht 2.py
@@ -3,5 +3,7 @@ cijferPROG = 8
 cijferCSN = 7
 gemiddelde = ((cijferICOR + cijferPROG + cijferCSN)/3)
 beloning = ((cijferICOR * 30) + (cijferPROG * 30) + (cijferCSN * 30))
-overzicht = 'Mijn cijfers (gemiddeld een  ) leveren een beloning van type(beloning) euro op!'
+line1 = int(gemiddelde)
+line2 = int(beloning)
+overzicht = "Mijn cijfers (gemiddeld een {} leveren een beloning van {} op!".format(line1, line2)
 print(overzicht)

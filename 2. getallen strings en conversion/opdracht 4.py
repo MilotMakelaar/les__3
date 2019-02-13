10a07697c2588e1042fa29e357affffdb30a1f48
Milot <milot.makelaar@student.hu.nl>
2019-02-13 14:20:26 +0100

    opdracht 4

---
+++ 2. getallen strings en conversion/opdracht 4.py
@@ -0,0 +1,5 @@
+uurloon = float(input("Wat verdien je per uur?"))
+uren = float(input("Hoeveel uur heb je gewerkt?"))
+totaal = (uurloon * uren)
+overzicht = str(uren)+' uur werken levert'+' '+str(totaal)+' Euro op'
+print(overzicht)
